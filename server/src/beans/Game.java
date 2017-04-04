6a057b4ad330717eb3156777344fc95f5a4fc6ac
Benjamin Riviere <benjamin.riviere@etu.utc.fr>
2017-04-04 18:15:25 +0200

    Define bean for Games.

--- server/src/beans/Game.java
+++ server/src/beans/Game.java
@@ -4,12 +4,67 @@ import java.io.Serializable;
 public class Game implements Serializable, Comparable<Game> {
 	private int id;
 	private String title;
+	private String console;
 	private float price;
+	private String releaseDate;
+	private int stock;
 	
-		
+	public Game() {}
 	
-	public Game() {
-		// TODO Auto-generated constructor stub
+	public Game(int id, String title, float price, String releaseDate, int stock) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.price = price;
+		this.releaseDate = releaseDate;
+		this.stock = stock;
 	}
+
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getConsole() {
+		return console;
+	}
+	public void setConsole(String console) {
+		this.console = console;
+	}
+	
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate){
+		this.releaseDate = releaseDate;
+	}
+	
+	public int getStock() {
+		return stock;
+	}
+	public void setStock(int stock) {
+		this.stock = stock;
+	}
+
+	@Override
+	public int compareTo(Game g) {
+		return this.id - g.id;
+	}	
 
 }
--- server/src/dao/Games.java
+++ server/src/dao/Games.java
@@ -8,7 +8,32 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import bdd.DatabaseConnection;
 import beans.Game;
 
 public class Games {
+	
+	public static int count(){
+	
+		int counter = 0;
+		Connection cnx = null;
+		
+		try {
+			cnx = DatabaseConnection.getInstance().getCnx();
+		
+			String sql = "SELECT COUNT(*) FROM games;";
+			PreparedStatement ps = cnx.prepareStatement(sql);
+			ResultSet res = ps.executeQuery();
+			
+			while(res.next()){
+				counter = res.getInt("COUNT(*)");
+				break;
+			}	
+		}catch (SQLException e) {
+			e.printStackTrace();
+			return -1;
+		}
+		
+		return counter;
+	}
 }
