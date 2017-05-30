2df276f8c95c6a904918e6ab30f8bca8face0643
Benjamin Riviere <benjamin.riviere@etu.utc.fr>
2017-05-30 21:40:38 +0200

    Build dialog form interface.

--- client/web/views/admin.games.php
+++ client/web/views/admin.games.php
@@ -11,10 +11,50 @@
 </button>
 
 <!-- The dialog to add a game -->
-<dialog class="mdl-dialog" id="add-dialog">
+<dialog class="mdl-dialog add-dialog" id="add-dialog">
 	<h4 class="mdl-dialog__title">Ajouter un jeu</h4>
 	<div class="mdl-dialog__content">
-		Formulaire pour ajouter un jeu
+		<div class="mdl-grid">
+			<div class="mdl-cell mdl-cell--12-col">
+				Tous les champs sont obligatoires à l'exception de l'éditeur, de la description, du lien de la jaquette ainsi que des genres.
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="dialog-game-title">
+				<label class="mdl-textfield__label" for="dialog-game-title">Titre *</label>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="dialog-game-console">
+				<label class="mdl-textfield__label" for="dialog-game-console">Console *</label>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="dialog-game-release-date">
+				<label class="mdl-textfield__label" for="dialog-game-release-date">Date de sortie *</label>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="number" min="0" step="0.01" id="dialog-game-release-price">
+				<label class="mdl-textfield__label" for="dialog-game-release-price">Prix *</label>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" id="dialog-game-release-publisher">
+				<label class="mdl-textfield__label" for="dialog-game-release-publisher">Editeur</label>
+			</div>
+			<div class="mdl-cell mdl-cell--4-col mdl-cell--12-col-phone mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="number" min="0" id="dialog-game-release-stock">
+				<label class="mdl-textfield__label" for="dialog-game-release-stock">Stock *</label>
+			</div>
+			<div class="mdl-cell mdl-cell--12-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="text" pattern="((\s)*[a-zA-Z0-9\s]+\|?)+" id="dialog-game-release-genres">
+				<label class="mdl-textfield__label" for="dialog-game-release-genres">Genres</label>
+			</div>
+			<div class="mdl-cell mdl-cell--12-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<input class="mdl-textfield__input" type="url" id="dialog-game-release-cover">
+				<label class="mdl-textfield__label" for="dialog-game-release-cover">URL de la jaquette</label>
+			</div>
+			<div class="mdl-cell mdl-cell--12-col mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+				<textarea class="mdl-textfield__input" type="text" rows="5" id="dialog-game-description"></textarea>
+				<label class="mdl-textfield__label" for="dialog-game-description">Description</label>
+			</div>			
+		</div>		
 	</div>
 	<div class="mdl-dialog__actions">
 		<button type="button" class="mdl-button">Ajouter</button>
